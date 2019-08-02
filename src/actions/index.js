bc177ff7a22f21966523b0dae7f73a03625c05e5
midoriver1 <midoriiiiii9866@gmail.com>
2019-08-02 18:09:35 +0900

    implemnt delete feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,17 +2,23 @@ import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENTS = 'CREATE_EVENTS'
+export const DELETE_EVENTS = 'DELETE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
-export const readEvents = () => async (dispatch) => {
+export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch({type: 'READ_EVENTS', response})
 };
 
-export const postEvent = values => async (dispatch) => {
+export const postEvent = values => async dispatch => {
     const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
     dispatch({type: 'CREATE_EVENTS', response})
+};
+
+export const deleteEvent = id => async dispatch => {
+    await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({type: 'DELETE_EVENTS', id})
 };
 
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,13 @@
-import {READ_EVENTS} from "../actions";
+import {DELETE_EVENTS, READ_EVENTS} from "../actions";
 import _ from 'lodash'
 
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENTS:
+            delete events[action.id]
+            return {...events}
         default:
             return events
 
