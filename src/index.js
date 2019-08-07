0c74ea9698b4b1654fe738daaa9e967ca682fcd9
midoriver1 <midoriiiiii9866@gmail.com>
2019-08-07 15:17:20 +0900

    implement eventsShow

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new'
 import EventsShow from './components/events_show'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+
 import * as serviceWorker from './serviceWorker';
-import {composeWithDevTools} from "redux-devtools-extension";
 
 const enhancer = process.env.NODE_ENV === 'development' ?
     composeWithDevTools(applyMiddleware(thunk)) : (applyMiddleware(thunk))
